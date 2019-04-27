4a8a520d7c4450288a512c6e05d7158fec19c466
cnagbim <cnagbim@gmail.com>
2019-04-26 23:02:55 -0500

    script update

--- tha4_full_script.R
+++ tha4_full_script.R
@@ -1,10 +1,12 @@
-#the purpose of this script is understand the correlation between county level
-#vulnerability and rooftop solar in Texas
+#the purpose of this script is to 1) understand the correlation between county level
+#vulnerability and rooftop solar energy production potential in Texas
+#and 2) understand the correlation between county level vulnerability and 
+#the amount of buildings with rooftop solar potential in Texas
 #we will first import and clean data
 svi_raw<-read.csv("~/Github/THA4_DataMgmt/Texas_COUNTY.csv", header=TRUE,na.strings="")
-
+head(svi_raw)
 pv_lmi_raw<-read.csv("~/Github/THA4_DataMgmt/pv_rooftop_county_lmi_pct_gwh.csv", header = TRUE,na.string="")
-
+head(pv_lmi_raw)
 #filter pv_lmi to include only TX counties
 #load tidyverse before filtering
 library(tidyverse)
@@ -12,7 +14,7 @@ library(tidyverse)
 #filter so only contains TX county values
 pv_lmi_tx_v1<-filter(pv_lmi_raw,pv_lmi_raw$state_abbr=="TX")
 
-
+head(pv_lmi_tx_v1)
 #We are only interested in the pcnt gwh which 
 #is the percentage of total energy production that rests on LMI households
 #use data.frame to slect those variables
@@ -37,9 +39,28 @@ names(pv_lmi_v2)<-pv_lmi_headr
 #all.y=True ensures that only observations with matches are included in new dataset
 svi_pv_lmi_v1<-merge(x=svi_v1,y=pv_lmi_v2,by="cnty",all.y=TRUE)
 
-#correlation and correlation test
-cor(svi_pv_lmi_v1$svi_score,svi_pv_lmi_v1$lmi_pcnt_pv_potential)
+#check datatype to ensure svi_score, lmi_pcnt_bldg, and lmi_pcnt_pv_potential
+str(svi_pv_lmi_v1)
+
+#correlation and correlation test 1) svi and potential
 cor.test(svi_pv_lmi_v1$svi_score,svi_pv_lmi_v1$lmi_pcnt_pv_potential)
 
-cor(svi_pv_lmi_v1$svi_score,svi_pv_lmi_v1$lmi_pcnt_bldgs)
+#Results show that there is a moderate positive correlation r=0.52 
+#Correlation between the social vulnerability index and the percentage of pv solar 
+#energy potential on lmi resdidantial buidings p value<<<0.05 which is significant
+
+#export correlation
+write.csv(cor(svi_pv_lmi_v1$svi_score,svi_pv_lmi_v1$lmi_pcnt_pv_potential),
+          file="corr_svi_potential.csv")
+
+
+
+#correlation and correlation test 2) svi and bldg
 cor.test(svi_pv_lmi_v1$svi_score,svi_pv_lmi_v1$lmi_pcnt_bldgs)
+
+#Results show that there is a moderate positive correlation r=0.52 
+#Correlation between the social vulnerability index and the percentage of lmi resdidantial
+#buidings suitable for pv solar. p value<<<0.05 which is significant
+
+#export correlation
+write.csv(cor(svi_pv_lmi_v1$svi_score,svi_pv_lmi_v1$lmi_pcnt_bldgs),file="corr_svi_bldgs.csv")
